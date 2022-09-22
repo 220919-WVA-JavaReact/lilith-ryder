838ced7dce1eebce744a605070675c95d5e84609
Hek80 <pikachu94@gmail.com>
2022-09-22 18:16:39 -0400

    first draft of persistent data script

---
+++ assignments/week1/persist-data-exercise/udata.sh
@@ -0,0 +1,32 @@
+#!/bin/bash/
+
+exec < ${1}
+read header
+
+while IFS="," read -r fname lname email usr pwd
+do
+  echo
+  echo $fname $lname $email $usr $pwd
+  echo "+---------------------------------+"
+done
+
+while true; do
+
+read -p "Enter new data? Y/N: " YN
+
+if [[ "$YN" == "Y" ]]
+then
+  read -p "input First Name: " fname1 ;
+  read -p "input Last Name: " lname1 ;
+  read -p "input email address: " email1 ;
+  read -p "input username: " usr1 ;
+  read -p "input password: " pwd1 ;
+  echo "$fname1,$lname1,$email1,$usr1,$pwd1" >> MOCK_DATA.csv ;
+  echo "New entry added to file." ;
+
+elif [[ "$YN" == "N" ]]
+then
+  exit 0 ;
+fi
+
+done
