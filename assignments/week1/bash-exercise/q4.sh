ccef10174997c8a4d2b23c0c9e715c9d3211ab44
Hek80 <pikachu94@gmail.com>
2022-09-23 16:39:16 -0400

    commit q4

---
+++ assignments/week1/bash-exercise/q4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo
+pwd
+echo
+whoami
+echo
+echo "Today is: " `date ;
+echo "No. of users logged in: " `who --count ; #for all intents and purposes this line should work, it works in the console by itself, no idea why it won't run in this script.
