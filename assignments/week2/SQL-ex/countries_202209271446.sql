62cd21c7a9e872de5bc1eb85f91b7d381b9c7214
Hek80 <pikachu94@gmail.com>
2022-09-27 14:47:13 -0400

    pushing SQL file for exercise

---
+++ assignments/week2/SQL-ex/countries_202209271446.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.countries ("name",population,countrycode,capital,spaceprogram) VALUES
+	 ('USA',332403650,1,'Washington, DC',true),
+	 ('Japan',125614358,81,'Tokyo',true),
+	 ('Ireland',5060845,353,'Dublin',false),
+	 ('Germany',84381438,49,'Berlin',true),
+	 ('Russia',146073999,7,'Moscow',true);
