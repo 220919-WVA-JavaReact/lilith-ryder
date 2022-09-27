325070dd5eff1578f46ad784553a223325e12acb
Hek80 <pikachu94@gmail.com>
2022-09-27 14:50:14 -0400

    pushing updates

--- assignments/week1/bash-exercise/q4.sh
+++ assignments/week1/bash-exercise/q4.sh
@@ -5,5 +5,5 @@ pwd
 echo
 whoami
 echo
-echo "Today is: " `date ;
-echo "No. of users logged in: " `who --count ; #for all intents and purposes this line should work, it works in the console by itself, no idea why it won't run in this script.
+echo "Today is: $(date)" ;
+echo "No. of users logged in:  $(who --count) " ; #for all intents and purposes this line should work, it works in the console by itself, no idea why it won't run in this script.
--- assignments/week1/bash-exercise/q5.sh
+++ assignments/week1/bash-exercise/q5.sh
@@ -4,7 +4,7 @@ echo "Enter 10 numbers"
 
 read N1 N2 N3 N4 N5 N6 N7 N8 N9 N10
 
-for [ $N1, $N2, $N3, $N4, $N5, $N6, $N7, $N8, $N9, $N10 ]=input
+for ($N1, $N2, $N3, $N4, $N5, $N6, $N7, $N8, $N9, $N10) = input
 do
 
 if [ $input -ge 0 ]
