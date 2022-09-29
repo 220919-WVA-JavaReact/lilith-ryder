d7a3609973450ff07071090f9d1192a8066cc80d
Hek80 <pikachu94@gmail.com>
2022-09-29 18:05:47 -0400

    pushing what I got done on java exercise

---
+++ assignments/week2/Java-ex/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld{
+
+	public static void main(String[] args) {
+
+    	System.out.println("Hello World");
+	
+	int number = 1000;
+
+	byte smaller = 50;
+
+	boolean value = true;
+
+	System.out.println(number);
+	System.out.println(smaller);
+	System.out.println(value);
+}
+
+}
