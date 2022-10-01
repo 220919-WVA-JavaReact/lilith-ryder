1444cbb89dfc090475b90cb2fa49dc65d78921ca
Hek80 <pikachu94@gmail.com>
2022-10-01 15:43:08 -0400

    pushing second iteration of calculator that actially works

--- assignments/week2/Java-ex/JavaCalc/calc.java
+++ assignments/week2/Java-ex/JavaCalc/calc.java
@@ -1,39 +1,78 @@
+import java.util.Scanner;
+
 public class calc {
 
     public static void main(String[] args) {
-        System.out.println("Java Calculator v0.1");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Java Calculator v0.2");
         System.out.println("Please enter your operator followed by two numbers.");
-        System.out.println("For a list of operators, enter 'ophelp'.");
+        System.out.println("For a list of operators, enter 'ophelp 0 0'.");
 
-        String operation;
-        float n1;
-        float n2;
+        String input2 = input.nextLine();
 
-        if (operation.equals("ophelp")) {
-            System.out.println("enter 'SUM' to add two numbers.");
-            System.out.println("enter 'DIF' to subtract the second number from the first.");
-            System.out.println("enter 'PRD' to multiply two numbers.");
-            System.out.println("enter 'QUO' to divide the first number by the second.");
-            System.out.println("enter 'exit' to quit.");
-        } else if (operation.equals("SUM")) {
+        String[] inputParse = input2.split(" ");
+
+        String operation = inputParse[0];
+        float n1 = Float.parseFloat(inputParse[1]);
+        float n2 = Float.parseFloat(inputParse[2]);
+        float result;
+
+            switch (operation) {
+                case "SUM":
+                    result = n1 + n2;
+                    System.out.println(result);
+                    break;
+
+                case "DIF":
+                    result = n1 - n2;
+                    System.out.println(result);
+                    break;
+
+                case "PRD":
+                    result = n1 * n2;
+                    System.out.println(result);
+                    break;
+
+                case "QUO":
+                    result = n1 / n2;
+                    System.out.println(result);
+                    break;
+
+                case "ophelp":
+                    System.out.println("valid operations: [ SUM, DIF, PRD, QUO ]");
+                    break;
+
+                default:
+                    System.out.println("invalid operation!");
+                    break;
+
+        /* for (operation, n1, n2;) {
+        if (operation.equals("?")) {
+            System.out.println("enter '+' to add two numbers.");
+            System.out.println("enter '-' to subtract the second number from the first.");
+            System.out.println("enter '*' to multiply two numbers.");
+            System.out.println("enter '/' to divide the first number by the second.");
+            System.out.println("enter 'q' to quit.");
+        } else if (operation.equals("+")) {
             float result = (n1 += n2);
-            return result;
-        } else if (operation.equals("DIF")) {
+
+        } else if (operation.equals("-")) {
             float result = n1 -= n2;
-            return result;
-        } else if (operation.equals("PRD")) {
+
+        } else if (operation.equals("*")) {
             float result = n1 * n2;
-            return result;
+
         }
-        else if (operation.equals("QUO")) {
+        else if (operation.equals("/")) {
             float result = n1 / n2;
-            return result;
+
         }
-        else if (operation.equals("exit")) {
+        else if (operation.equals("q")) {
             break;
         }
         else {
-            System.out.println("invalid operator!");
+            System.out.println("invalid operator!"); */
+            }
         }
-    }
 }
